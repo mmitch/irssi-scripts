1f4eb36d674772263decf92b749962f12953203f
Christian Garbs <mitch@cgarbs.de>
2008-02-15 23:19:51 +0100

    fix 4chanarchive.org

--- 4chan.pl
+++ 4chan.pl
@@ -148,7 +148,7 @@ sub check_for_link {
 	$board = $2;
 	$file = $3;
 	$url =~ s|/src.cgi/|/src/|;
-    } elsif ($message =~ m|(http://4chanarchive\.org/images/(?:[a-z]+/)\d+/(\d+\.[a-z]+))|) {
+    } elsif ($message =~ m|(http://4chanarchive\.org/images/(?:[a-z]+/)?\d+/(\d+\.[a-z]+))|) {
 	$chan = '4chanarchive';
 	$url = $1;
 	$board = '?';
